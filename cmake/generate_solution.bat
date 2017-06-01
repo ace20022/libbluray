4bb5713f91b04363599ed269b77e2108ba7990e7
ace20022 <ace20022@ymail.com>
2017-06-01 23:17:47 +0200

    ant

--- cmake/generate_solution.bat
+++ cmake/generate_solution.bat
@@ -3,5 +3,5 @@ REM IF EXIST "%~dp0\build" rmdir "%~dp0\build" /S /Q
 REM md build
 IF NOT EXIST "%~dp0\build" md build
 cd build
-cmake .. -G "Visual Studio 14" -DANTEXE=D:\Code\ant\bin\ant
+cmake .. -G "Visual Studio 14" -DANTEXE="C:\XBMC\blueray_stuff\apache-ant-1.9.0\bin\ant"
 cd ..
